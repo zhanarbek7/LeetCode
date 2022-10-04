4f4ea07c7f949dd76efed42b4c6a04293aa98628
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2022-10-04 22:29:30 +0600

    Time: 5 ms (98.46%), Space: 55 MB (90.18%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums==null || nums.length==0)
-        return false;
-        HashSet<Integer> set = new HashSet<>();
-        for(int i : nums){
-            if(set.contains(i)){
+        HashSet<Integer> numbers = new HashSet<>();
+        for(int i: nums){
+            if(numbers.contains(i)){
                 return true;
             }
-            set.add(i);
+            numbers.add(i);
         }
-        return false; 
+        return false;
     }
 }
