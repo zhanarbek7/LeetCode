19bf3189d7100f7512a2b0b28f8791c91c4345e1
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2022-10-10 21:36:10 +0600

    Time: 2 ms (92.16%), Space: 45.7 MB (95.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,14 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            if (map.containsKey(num)) {
-                map.replace(num, map.get(num), map.get(num) + 1);
-            } else map.put(num, 0);
-        }
-        Comparator<Integer> valueComparator = (k1, k2) -> map.get(k2).compareTo(map.get(k1));
-        TreeMap<Integer,Integer> treeMap = new TreeMap<>(valueComparator);
-        treeMap.putAll(map);
-        return treeMap.entrySet().iterator().next().getKey();
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
